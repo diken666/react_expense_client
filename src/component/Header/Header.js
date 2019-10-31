04b4b23546c44c64a6a7c8627da8215116b3a791
diken666 <diken666@163.com>
2019-10-31 21:53:49 +0800

    update

---
+++ src/component/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from './Header.module.scss'
+
+export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div className={style.header}>
+
+            </div>
+        )
+    }
+}
