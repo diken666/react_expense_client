112074e8b396cccd7453367969c1334e705cfc08
diken666 <diken666@163.com>
2019-11-05 21:49:37 +0800

    update 11/5

--- src/App.js
+++ src/App.js
@@ -2,35 +2,84 @@ import React from 'react';
 import Header from "./component/Header/Header";
 import MenuBox from "./component/MenuBox/MenuBox";
 import RightBox from "./component/RightBox/RightBox";
-import { Layout } from 'antd';
+import { Layout, Button } from 'antd';
 import axios from 'axios';
 
-
+import style from './App.module.scss';
 
 class App extends React.Component{
     constructor ( props ) {
         super(props);
         this.state = {
-
+            userState: 'loading'
         }
     }
-    async componentDidMount() {
-        console.log(await this.getUserState() )
+    async componentDidMount(){
+        let state = await this.getUserState();
+        this.setState({
+            userState: state.data.state,
+            loginLoading: false
+        })
     }
-    async getUserState(){
-        let state = await axios.get('http://localhost:3000/getuserstate');
-        return state;
+    getUserState(){
+        return axios.get('http://localhost:3000/getuserstate');
+    }
+    loginClick(){
+        console.log('123');
+        this.setState({
+            loginLoading: true
+        })
+    }
+    stateRender(state){
+        switch (state) {
+            case 'loading':
+                return (
+                    <div className={style.loadingBox}>
+                        <i className={style.loading} />
+                        <span className={style.loadingTxt}>加载中</span>
+                    </div>
+                );
+            case 'logout':
+                return (
+                    <div className={style.logout}>
+                        <div className={style.loginBox}>
+                            <div className={style.loginTitle}>登陆</div>
+                            <form action="">
+                                <div className={style.inputItem}>
+                                    <i className={[style.icon, style.userLogin].join(" ")} />
+                                    <input id="account" className={style.input} type="text"/>
+                                </div>
+                                <div className={style.inputItem}>
+                                    <i className={[style.icon, style.password].join(" ")} />
+                                    <input id="password" className={style.input} type="password"/>
+                                </div>
+                                <div className={style.loginBtn}>
+                                    <Button loading={this.state.loginLoading} type="primary" block size={"large"} onClick={()=>this.loginClick()} >登陆</Button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                );
+            case 'login':
+                return (
+                    <Layout>
+                        <Header />
+                        <Layout>
+                            <MenuBox />
+                            <RightBox />
+                        </Layout>
+                    </Layout>
+                );
+            default:
+                return <div>logout</div>
+        }
     }
     render() {
         return (
             <div className="container">
-                <Layout>
-                    <Header />
-                    <Layout>
-                        <MenuBox />
-                        <RightBox />
-                    </Layout>
-                </Layout>
+                {
+                    this.stateRender(this.state.userState)
+                }
             </div>
         )
     }
