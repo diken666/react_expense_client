a45999ec3b3d77cb66bbcab97d305debcac70168
diken <diken666@163.com>
2019-11-14 17:36:48 +0800

    update 11/14

---
+++ src/component/Table/Table.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './Table.module.scss';
+
+export default class Table extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
