4f04d10899a7f3e2203260202d385413219581ff
diken666 <diken666@163.com>
2020-02-28 17:27:42 +0800

    update

--- src/component/SearchTable/SearchTable.js
+++ src/component/SearchTable/SearchTable.js
@@ -102,25 +102,6 @@ export default class SearchTable extends React.Component {
                   onChange={this.onChange} 
                   bordered 
                 />
-                <div className={style.searchItem}>
-                    {/* <span>查看内容：</span>
-                    <Select defaultValue="allRoomData" style={{ width: 130 }} onChange={()=>this.selectChange()}>
-                        <Option value="allRoomData">所有房间数据</Option>
-                        <Option value="roomData">单个房间数据</Option>
-                        <Option value="allPersonData">所有住户数据</Option>
-                        <Option value="personData">单个用户数据</Option>
-                    </Select> */}
-
-                    {/*<Search placeholder="input search text" onSearch={value => console.log(value)} enterButton />*/}
-                </div>
-                {/* <div className={style.searchItem}>
-                    <span>日期选择：</span>
-                    <Select defaultValue="allRoomData" style={{ width: 240 }} onChange={()=>this.selectChange()}>
-                        <Option value="allRoomData">2019-9-27 至 2019-10-27</Option>
-                        <Option value="roomData">2019-10-27 至 2019-11-25</Option>
-                        <Option value="allPersonData">2019-11-25 至 2019-12-25</Option>
-                    </Select>
-                </div> */}
             </div>
         )
     }
